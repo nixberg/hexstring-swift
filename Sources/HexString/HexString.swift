3d29e4fb64539fc9851b65e11973c1f29064a0da
nixberg <k.nixberg@gmail.com>
2021-09-01 16:55:25 +0200

    Add ExpressibleByStringLiteral conformance

--- Sources/HexString/HexString.swift
+++ Sources/HexString/HexString.swift
@@ -1,8 +1,22 @@
 import Algorithms
 
 @propertyWrapper
-public struct HexString: Decodable {
+public struct HexString: Decodable, ExpressibleByStringLiteral {
+    public typealias StringLiteralType = String
+    
     public let wrappedValue: [UInt8]
+    
+    public init(stringLiteral hexString: StringLiteralType) {
+        let hexString = hexString.filter { !$0.isWhitespace }
+        
+        precondition(hexString.count.isMultiple(of: 2))
+        
+        wrappedValue = hexString
+            .chunks(ofCount: 2)
+            .compactMap { UInt8($0, radix: 16) }
+        
+        precondition(wrappedValue.count == hexString.count / 2)
+    }
     
     public init(from decoder: Decoder) throws {
         let container = try decoder.singleValueContainer()
