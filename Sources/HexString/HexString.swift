cbf3d8f7af500d7476d1589b285455b9a4b23419
nixberg <k.nixberg@gmail.com>
2021-09-01 17:00:09 +0200

    Allow whitespace characters and refactor

--- Sources/HexString/HexString.swift
+++ Sources/HexString/HexString.swift
@@ -20,18 +20,18 @@ public struct HexString: Decodable, ExpressibleByStringLiteral {
     
     public init(from decoder: Decoder) throws {
         let container = try decoder.singleValueContainer()
-        let hexString = try container.decode(String.self)
+        let hexString = try container.decode(String.self).filter { !$0.isWhitespace }
         
         guard hexString.count.isMultiple(of: 2) else {
             throw DecodingError.dataCorruptedError(in: container, debugDescription: "")
         }
         
-        guard hexString.allSatisfy(\.isHexDigit) else {
-            throw DecodingError.dataCorruptedError(in: container, debugDescription: "")
-        }
-        
         wrappedValue = hexString.chunks(ofCount: 2).compactMap {
             UInt8($0, radix: 16)
+        }
+        
+        guard wrappedValue.count == hexString.count / 2 else {
+            throw DecodingError.dataCorruptedError(in: container, debugDescription: "")
         }
     }
 }
