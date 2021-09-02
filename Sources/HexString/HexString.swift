ef628ff0564cf699ae23f85d5d4a49b5531a73b8
nixberg <k.nixberg@gmail.com>
2021-09-02 17:15:05 +0200

    Make [UInt8] conform to ExpressibleByStringLiteral

--- Sources/HexString/HexString.swift
+++ Sources/HexString/HexString.swift
@@ -1,37 +1,48 @@
 import Algorithms
 
 @propertyWrapper
-public struct HexString: Decodable, ExpressibleByStringLiteral {
-    public typealias StringLiteralType = String
-    
+public struct HexString: Decodable {
     public let wrappedValue: [UInt8]
-    
-    public init(stringLiteral hexString: StringLiteralType) {
-        let hexString = hexString.filter { !$0.isWhitespace }
-        
-        precondition(hexString.count.isMultiple(of: 2))
-        
-        wrappedValue = hexString
-            .chunks(ofCount: 2)
-            .compactMap { UInt8($0, radix: 16) }
-        
-        precondition(wrappedValue.count == hexString.count / 2)
-    }
     
     public init(from decoder: Decoder) throws {
         let container = try decoder.singleValueContainer()
-        let hexString = try container.decode(String.self).filter { !$0.isWhitespace }
         
-        guard hexString.count.isMultiple(of: 2) else {
+        let strippedHexString = try container.decode(String.self).filter { !$0.isWhitespace }
+        
+        guard strippedHexString.count.isMultiple(of: 2) else {
             throw DecodingError.dataCorruptedError(in: container, debugDescription: "")
         }
         
-        wrappedValue = hexString.chunks(ofCount: 2).compactMap {
-            UInt8($0, radix: 16)
-        }
+        wrappedValue = strippedHexString
+            .chunks(ofCount: 2)
+            .compactMap { UInt8($0, radix: 16) }
         
-        guard wrappedValue.count == hexString.count / 2 else {
+        guard wrappedValue.count == strippedHexString.count / 2 else {
             throw DecodingError.dataCorruptedError(in: container, debugDescription: "")
         }
     }
+}
+
+extension Array: ExpressibleByStringLiteral where Element == UInt8 {
+    public typealias StringLiteralType = String
+    
+    public init(stringLiteral hexString: StringLiteralType) {
+        let strippedHexString = hexString.filter { !$0.isWhitespace }
+        
+        precondition(strippedHexString.count.isMultiple(of: 2))
+        
+        self = strippedHexString
+            .chunks(ofCount: 2)
+            .compactMap { UInt8($0, radix: 16) }
+        
+        precondition(count == strippedHexString.count / 2)
+    }
+}
+
+extension Array: ExpressibleByUnicodeScalarLiteral where Element == UInt8 {
+    public typealias UnicodeScalarLiteralType = String
+}
+
+extension Array: ExpressibleByExtendedGraphemeClusterLiteral where Element == UInt8 {
+    public typealias ExtendedGraphemeClusterLiteralType = String
 }
--- Tests/HexStringTests/HexStringTests.swift
+++ Tests/HexStringTests/HexStringTests.swift
@@ -1,6 +1,38 @@
+import HexString
 import XCTest
-@testable import HexString
 
 final class HexStringTests: XCTestCase {
-    func testExample() { }
+    func testDecodable() throws {
+        struct Struct: Decodable {
+            @HexString var bytes: [UInt8]
+        }
+        
+        let decoded = try JSONDecoder()
+            .decode(Struct.self, from: """
+            {
+                "bytes": "dead      b eef"
+            }
+            """.data(using: .ascii)!)
+        
+        XCTAssertEqual(decoded.bytes, [0xde, 0xad, 0xbe, 0xef])
+        
+        XCTAssertThrowsError(try JSONDecoder()
+                                .decode(Struct.self, from: """
+                                {
+                                    "bytes": "de:ad      b eef"
+                                }
+                                """.data(using: .ascii)!))
+        
+        XCTAssertThrowsError(try JSONDecoder()
+                                .decode(Struct.self, from: """
+                                {
+                                    "bytes": "dead      b ee"
+                                }
+                                """.data(using: .ascii)!))
+    }
+    
+    func testExpressibleByStringLiteral() {
+        let bytes: [UInt8] = "dead      b eef"
+        XCTAssertEqual(bytes, [0xde, 0xad, 0xbe, 0xef])
+    }
 }
